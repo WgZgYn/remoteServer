cacc43b3524c70dc75f489656e4c1a48f4ce5093
wzy <2049562063@qq.com>
2024-11-01 23:15:03 +0800

    add Cors

--- src/main/java/org/scu301/remoteserver/config/WebConfig.java
+++ src/main/java/org/scu301/remoteserver/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.scu301.remoteserver.config;
 
 import org.scu301.remoteserver.security.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/api/login", "/api/signup", "/test/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
