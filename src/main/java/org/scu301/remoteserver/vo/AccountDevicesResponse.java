bae9043bb8083983e340372366788de577ef271f
wzy <2049562063@qq.com>
2024-11-06 19:12:33 +0800

    fix name

--- src/main/java/org/scu301/remoteserver/vo/AccountDevicesResponse.java
+++ src/main/java/org/scu301/remoteserver/vo/AccountDevicesResponse.java
@@ -2,5 +2,5 @@ package org.scu301.remoteserver.vo;
 
 import java.util.List;
 
-public record AccountDevicesResponse(AccountInfo account_info, List<HouseDevicesResponse> house_devices) {
+public record AccountDevicesResponse(AccountInfo account_info, List<HouseDevicesResponse> houses_devices) {
 }
