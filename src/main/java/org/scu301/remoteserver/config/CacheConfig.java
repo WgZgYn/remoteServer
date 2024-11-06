a96d8c724ad820cc8803efc219bb08456938fbb9
wzy <2049562063@qq.com>
2024-11-06 18:13:31 +0800

    add caffeine cache

--- src/main/java/org/scu301/remoteserver/config/CacheConfig.java
+++ src/main/java/org/scu301/remoteserver/config/CacheConfig.java
@@ -1,10 +1,14 @@
 package org.scu301.remoteserver.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 
 @Configuration
@@ -14,5 +18,17 @@ public class CacheConfig {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10)) // 设置缓存过期时间
                 .disableCachingNullValues();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        // 定义默认缓存配置
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100));
+
+        return cacheManager;
     }
 }
