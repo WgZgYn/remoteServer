005752ac61621d1b8bc3f3580bb71bd6ed7d37ca
wzy <2049562063@qq.com>
2024-11-06 18:12:31 +0800

    add more curd method

--- src/main/java/org/scu301/remoteserver/repository/AccountRepository.java
+++ src/main/java/org/scu301/remoteserver/repository/AccountRepository.java
@@ -1,7 +1,5 @@
 package org.scu301.remoteserver.repository;
 
-import org.scu301.remoteserver.dto.AccountId;
-import org.scu301.remoteserver.dto.HouseId;
 import org.scu301.remoteserver.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,9 +12,9 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     Optional<Account> findAccountByUsername(String username);
     boolean existsAccountByUsername(String username);
 
-    @Query("SELECT new org.scu301.remoteserver.dto.AccountId(a.id) FROM Account a")
-    List<AccountId> findAllAccountId();
+    @Query("SELECT a.id FROM Account a")
+    List<Integer> findAllAccountId();
 
-    @Query("SELECT new org.scu301.remoteserver.dto.HouseId(m.house.id) FROM Member m WHERE m.account.id = :id")
-    List<HouseId> findAllHouseIdById(@Param("id") Integer id);
+    @Query("SELECT m.house.id FROM Member m WHERE m.account.id = :id")
+    List<Integer> findAllHouseIdById(@Param("id") Integer id);
 }
--- src/main/java/org/scu301/remoteserver/repository/MemberRepository.java
+++ src/main/java/org/scu301/remoteserver/repository/MemberRepository.java
@@ -1,11 +1,22 @@
 package org.scu301.remoteserver.repository;
 
+import org.scu301.remoteserver.entity.House;
 import org.scu301.remoteserver.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
     List<Member> findMembersByAccountId(Integer accountId);
+
     boolean existsByAccountIdAndHouseId(Integer accountId, Integer houseId);
+
+    @Query("SELECT m.house FROM Member m WHERE m.account.id = :accountId")
+    List<House> findHousesByAccountId(@Param("accountId") Integer accountId);
+
+    @Query("SELECT m.house FROM Member m WHERE m.account.id = :accountId AND m.house.id = :houseId")
+    Optional<House> findHouseByAccountIdAndHouseId(@Param("accountId") Integer accountId, @Param("houseId") Integer houseId);
 }
