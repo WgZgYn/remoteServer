89ae4ae8a5d4faf9930b9e526ef6aa0ffe9e7b2b
wzy <2049562063@qq.com>
2024-11-08 10:19:52 +0800

    split

---
+++ src/main/java/org/scu301/remoteserver/dto/http/AddAreaRequest.java
@@ -0,0 +1,4 @@
+package org.scu301.remoteserver.dto.http;
+
+public record AddAreaRequest(String area_name, Integer house_id) {
+}
---
+++ src/main/java/org/scu301/remoteserver/dto/http/AddHouseRequest.java
@@ -0,0 +1,4 @@
+package org.scu301.remoteserver.dto.http;
+
+public record AddHouseRequest(String house_name) {
+}
